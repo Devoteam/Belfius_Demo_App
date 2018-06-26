03d74059eb256712846d54fc70de4ef7245b525c
Mustafa Aittah <mustafa.aittah@devoteam.com>
2018-06-26 15:44:57 +0200

    base api url changed

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,14 +3,16 @@ import { environment } from '../environments/environment';
 // Base API URL
 const BASE_API_URL = environment.BASE_API_URL;
 
+const COMPANY_OPENSHIFT_PREFIX = 'company';
+const CAR_OPENSHIFT_PREFIX = 'car';
 // Cars
 const CAR_PREFIX = '/cars';
-export const GAT_ALL_CARS_FOR_COMPANY = `${BASE_API_URL}${CAR_PREFIX}/company/`;
-export const GET_CAR_DETAILS_BY_ID = `${BASE_API_URL}${CAR_PREFIX}/details/`;
+export const GAT_ALL_CARS_FOR_COMPANY = `${BASE_API_URL}${CAR_OPENSHIFT_PREFIX}${CAR_PREFIX}/company/`;
+export const GET_CAR_DETAILS_BY_ID = `${BASE_API_URL}${CAR_OPENSHIFT_PREFIX}${CAR_PREFIX}/details/`;
 
 // Companies
 const COMPANY_PREFIX = '/companies';
-export const GET_ALL_COMPANIES = `${BASE_API_URL}${COMPANY_PREFIX}/all`;
+export const GET_ALL_COMPANIES = `${BASE_API_URL}${COMPANY_OPENSHIFT_PREFIX}${COMPANY_PREFIX}/all`;
 
 export const RequestParams = {
     COMPANY_ID: 'companyId',
